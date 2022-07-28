f5be10d9229231f62c33daab242d079134aeb4bf
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-27 20:41:18 -0700

    added in connection.js funcitonality

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 // modules and other imports
 var mysql = require("mysql");
+require('dotenv').config();
 
 // create connection to sql
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3301,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+});
 
 module.exports = connection;
