8e5a93e9e5d8781b6fac5ca9378309b31f8198d7
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-27 18:07:08 -0700

    added sql code for schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,28 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 /* create employee table */
+CREATE Table employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
 
 /* create roles table */
+CREATE Table role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) default 0,
+    department_id INT UNSIGNED,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
 /* create departments table */
+CREATE Table department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    CONSTRAINT uc_name UNIQUE(name)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,14 @@
 USE employeeTrackerdb;
 
 /* insert into employees */
+INSERT INTO Employee (first_name, last_name, role_id) VALUES
+
+/* insert into employee (manager) */
+INSERT INTO Employee (first_name, last_name, role_id, manager_id) VALUES
 
 /* insert into roles */
+INSERT INTO Role (title, salary, department_id) VALUES
 
 /* insert into departments */
+INSERT INTO department (name) VALUES
+("engineering"), ("finance"), ("legal"), ("sales");
