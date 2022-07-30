12ea695d459d30c533727ac966b5f5b9e0f9ab43
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-30 12:54:53 -0700

    added in department.js; needs revision

--- db/department.js
+++ db/department.js
@@ -1,3 +1,49 @@
 // departments class
+class department {
+    constructor(connection) {
+        this.connection = connection;
+    }
 
+    getDepartments(cb) {
+        this.connection.query("
+        SELECT * 
+        FROM department"
+        , (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    };
+
+    getBudget(id, cb) {
+        this.connection.query("
+        SELECT SUM(role.salary)
+        FROM employee
+        INNER JOIN role on employee.role_id
+        AND department_id=?"
+        , id, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    };
+
+    addDepartment(department, cb) {
+        this.connection.query("
+        INSERT INTO department(name)
+        VALUES (?)"
+        , department, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    };
+
+    removeDepartments(id, cb) {
+        this.connection.query("
+        DELETE FROM department
+        WHERE id=?"
+        , id, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    };
+}
 module.exports = department;
