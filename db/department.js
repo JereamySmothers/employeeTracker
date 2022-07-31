efe100aeec57a20c5786e0761c9f70779db6deec
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-30 18:13:36 -0700

    fixed syntax errors for department and roles

--- db/department.js
+++ db/department.js
@@ -5,42 +5,42 @@ class department {
     }
 
     getDepartments(cb) {
-        this.connection.query("
+        this.connection.query(`
         SELECT * 
-        FROM department"
-        , (err, res) => {
+        FROM department
+        `, (err, res) => {
             if (err) throw err;
             cb(res);
         });
     };
 
     getBudget(id, cb) {
-        this.connection.query("
+        this.connection.query(`
         SELECT SUM(role.salary)
         FROM employee
         INNER JOIN role on employee.role_id
-        AND department_id=?"
-        , id, (err, res) => {
+        AND department_id=?
+        `, id, (err, res) => {
             if (err) throw err;
             cb(res);
         });
     };
 
     addDepartment(department, cb) {
-        this.connection.query("
+        this.connection.query(`
         INSERT INTO department(name)
-        VALUES (?)"
-        , department, (err, res) => {
+        VALUES (?)
+        `, department, (err, res) => {
             if (err) throw err;
             cb(res);
         });
     };
 
     removeDepartments(id, cb) {
-        this.connection.query("
+        this.connection.query(`
         DELETE FROM department
-        WHERE id=?"
-        , id, (err, res) => {
+        WHERE id=?
+        `, id, (err, res) => {
             if (err) throw err;
             cb(res);
         });
--- db/role.js
+++ db/role.js
@@ -5,27 +5,33 @@ class role {
     }
 
     getRoles(cb) {
-        this.connection.query("
-        SELECT title, salary, name AS department_name
-        FROM role LEFT JOIN department ON role.department_id = department.id"
-        , (err, res) => (
+        this.connection.query(`
+        SELECT title, salary, name 
+        AS department_name
+        FROM role 
+        LEFT JOIN department 
+        ON role.department_id = department.id
+        `, (err, res) => {
             if (err) throw err;
             cb(res);
         })
     };
 
     addRoles(role, cb) {
-        this.connection.query("
-        INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)"
-        , (err, res) => {
+        this.connection.query(`
+        INSERT INTO role(title, salary, department_id) 
+        VALUES (?, ?, ?)
+        `, (err, res) => {
             if (err) throw err;
             cb(res);
         })
     };
 
     removeRoles(id, cb) {
-        this.connection.query("
-        DELETE FROM role WHERE id=?", id, (err, res) => {
+        this.connection.query(`
+        DELETE FROM role 
+        WHERE id=? 
+        `,id, (err, res) => {
             if (err) throw err;
             cb(res);
         })
